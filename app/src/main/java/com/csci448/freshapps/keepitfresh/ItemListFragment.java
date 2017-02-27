07aff796c19780d74d7278afed1a430a0b2a6405
alex kurzon <akurzon@mymail.mines.edu>
2017-02-26 17:51:06 -0700

    item recycler view displays the stock items

--- app/src/main/java/com/csci448/freshapps/keepitfresh/ItemListFragment.java
+++ app/src/main/java/com/csci448/freshapps/keepitfresh/ItemListFragment.java
@@ -1,6 +1,7 @@
 package com.csci448.freshapps.keepitfresh;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +12,38 @@ import java.util.List;
 
 
 public class ItemListFragment extends Fragment {
+    private RecyclerView mCrimeRecyclerView;
+    private ItemAdapter mItemAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
         View view = inflater.inflate(R.layout.fragment_items_list, container, false);
 
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.items_list_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        updateUI();
+
         return view;
+    }
+
+    public void updateUI() {
+        StoredItems storedItems = StoredItems.getInstance();
+        List<Item> items = storedItems.getItemList();
+        if (mItemAdapter == null) {
+            mItemAdapter = new ItemAdapter(items);
+            mCrimeRecyclerView.setAdapter(mItemAdapter);
+        }
+        else {
+            mItemAdapter.notifyDataSetChanged();
+        }
     }
 
     private class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {

b2ab6dab73c8ee356a677c9ac88afe8f903dda4f
Nate Pohl <Nate Pohl>
2017-03-02 03:12:27 -0700

    Added a drawer layout that adds a sidebar, which in the future will allow us to navigate between lists using this menu.

--- app/src/main/java/com/csci448/freshapps/keepitfresh/ItemListActivity.java
+++ app/src/main/java/com/csci448/freshapps/keepitfresh/ItemListActivity.java
@@ -1,9 +1,29 @@
 package com.csci448.freshapps.keepitfresh;
 
+import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 
 public class ItemListActivity extends SingleFragmentActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    private String mActivityTitle;
+    private String[] mLocations = new String[4];
 
     @Override
     protected Fragment createFragment() {
@@ -14,9 +34,103 @@ public class ItemListActivity extends SingleFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        mLocations[0] = "All";
+        mLocations[1] = "Fridge";
+        mLocations[2] = "Freezer";
+        mLocations[3] = "Pantry";
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mLocations);
+        mDrawerList.setAdapter(mAdapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+                showToast(mLocations[pos] + " clicked!");
+            }
+        });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        setupDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
+    private void showToast(String string) {
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+    }
 
+    /**
+     * Code for the side bar.
+     * http://blog.teamtreehouse.com/add-navigation-drawer-android
+     */
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+                getSupportActionBar().setTitle("Navigation");
+                invalidateOptionsMenu();
+            }
+            public void onDrawerClosed(View v) {
+                super.onDrawerClosed(v);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
 
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle bundle) {
+        super.onPostCreate(bundle);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration config) {
+        super.onConfigurationChanged(config);
+        mDrawerToggle.onConfigurationChanged(config);
+    }
+
+    /**
+     * This code may become useful later
+     * Any item selected will open a new fragment depending on its position
+     * https://developer.android.com/training/implementing-navigation/nav-drawer.html
+     */
+    /*
+    //Swaps fragments in the main content view
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new PlanetFragment();
+        Bundle args = new Bundle();
+        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+    */
 }

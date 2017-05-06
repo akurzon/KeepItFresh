0a2c58b1155dec9d64253c8efd6167e2b60d4d18
alex kurzon <akurzon@mymail.mines.edu>
2017-05-06 13:36:11 -0600

    updated settings with a clear history button

--- app/src/main/java/com/csci448/freshapps/keepitfresh/SettingsActivity.java
+++ app/src/main/java/com/csci448/freshapps/keepitfresh/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.csci448.freshapps.keepitfresh;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -19,7 +21,6 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 

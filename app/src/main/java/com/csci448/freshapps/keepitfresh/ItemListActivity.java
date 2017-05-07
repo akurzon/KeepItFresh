1dae1d7f321cabeeefff2bef02a49c6eba03d1ee
alex kurzon <akurzon@mymail.mines.edu>
2017-05-06 23:27:59 -0600

    connected notification setting to the alarm

--- app/src/main/java/com/csci448/freshapps/keepitfresh/ItemListActivity.java
+++ app/src/main/java/com/csci448/freshapps/keepitfresh/ItemListActivity.java
@@ -6,8 +6,6 @@ import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 import android.view.View;
